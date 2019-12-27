fb32282a9d7cd5998b9d0a159cc5d8862b9b769f
BreadBytes <natesdevelopment@gmail.com>
2019-12-26 19:10:00 -0500

    Initial commit

---
+++ nateroth.me/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace nateroth.me.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
